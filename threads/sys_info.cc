cafbbcf3f386a759f2936e3f23faec946b1d05a5
Nicolas <nicolas@desktop>
2021-04-06 20:11:48 -0300

    Ejercicio 12

--- threads/sys_info.cc
+++ threads/sys_info.cc
@@ -3,7 +3,8 @@
 #include "filesys/directory_entry.hh"
 #include "filesys/file_system.hh"
 #include "filesys/raw_file_header.hh"
-
+#include "machine/mmu.hh"
+#include "machine/disk.hh"
 #include <stdio.h>
 
 
@@ -43,31 +44,33 @@ void SysInfo()
 
     printf("System information.\n");
     printf("\n\
-General:\n\
-  Nachos release: %s %s.\n\
-  Option definitions: %s\n",
+      General:\n\
+      Nachos release: %s %s.\n\
+      Option definitions: %s\n",
       PROGRAM, VERSION, OPTIONS);
     printf("\n\
-Memory:\n\
-  Page size: UNKNOWN bytes.\n\
-  Number of pages: UNKNOWN.\n\
-  Number of TLB entries: UNKNOWN.\n\
-  Memory size: UNKNOWN bytes.\n");
+      Memory:\n\
+      Page size: %u bytes.\n\
+      Number of pages: %u.\n\
+      Number of TLB entries: %u.\n\
+      Memory size: %u bytes.\n",
+      PAGE_SIZE, NUM_PHYS_PAGES, TLB_SIZE, MEMORY_SIZE);
     printf("\n\
-Disk:\n\
-  Sector size: UNKNOWN bytes.\n\
-  Sectors per track: UNKNOWN.\n\
-  Number of tracks: UNKNOWN.\n\
-  Number of sectors: UNKNOWN.\n\
-  Disk size: UNKNOWN bytes.\n");
+      Disk:\n\
+      Sector size: %u bytes.\n\
+      Sectors per track: %u.\n\
+      Number of tracks: %u.\n\
+      Number of sectors: %u.\n\
+      Disk size: %lu bytes.\n",
+      SECTOR_SIZE, SECTORS_PER_TRACK, NUM_TRACKS, NUM_SECTORS, sizeof(int) + NUM_SECTORS * SECTOR_SIZE);
     printf("\n\
-Filesystem:\n\
-  Sectors per header: %u.\n\
-  Maximum file size: %u bytes.\n\
-  File name maximum length: %u.\n\
-  Free sectors map size: %u bytes.\n\
-  Maximum number of dir-entries: %u.\n\
-  Directory file size: %u bytes.\n",
+      Filesystem:\n\
+      Sectors per header: %u.\n\
+      Maximum file size: %u bytes.\n\
+      File name maximum length: %u.\n\
+      Free sectors map size: %u bytes.\n\
+      Maximum number of dir-entries: %u.\n\
+      Directory file size: %u bytes.\n",
       NUM_DIRECT, MAX_FILE_SIZE, FILE_NAME_MAX_LEN,
       FREE_MAP_FILE_SIZE, NUM_DIR_ENTRIES, DIRECTORY_FILE_SIZE);
 }
